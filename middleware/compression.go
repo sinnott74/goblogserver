4d0b88d66205b15ddc1b2fd36847da21c5238f9e
Daniel Sinnott <sinnott74@hotmail.com>
2018-08-02 10:50:33 +0100

    Added export comment

--- middleware/compression.go
+++ middleware/compression.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-contrib/gzip"
 )
 
+// Compression middleware gzip the response before senting to the client
 func Compression() gin.HandlerFunc {
 	return gzip.Gzip(gzip.DefaultCompression)
 }
