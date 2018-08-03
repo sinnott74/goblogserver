88c890c3cbc84d06080ef02d1e6cd9f37519bbc3
Daniel Sinnott <sinnott74@hotmail.com>
2018-08-03 16:27:19 +0100

    Centralise EnvVars

--- database/database.go
+++ database/database.go
@@ -1,29 +1,19 @@
 package database
 
 import (
-	"os"
-
 	"github.com/iancoleman/strcase"
 	_ "github.com/lib/pq" // import postgres
+	"github.com/sinnott74/goblogserver/env"
 	"github.com/sinnott74/goblogserver/orm"
 )
 
 func Init() error {
 	config := orm.Config{
-		ConnURL:          connectionURL(),
+		ConnURL:          env.ConnectionURL(),
 		DriverName:       "postgres",
 		MaxConns:         2,
 		ToDBMapperFunc:   strcase.ToSnake,
 		FromDBMapperFunc: strcase.ToCamel,
 	}
 	return orm.Init(config)
-}
-
-// connectionURL get the database connection string from ENV Vars or used a default
-func connectionURL() string {
-	connectionString := os.Getenv("POSTGRES_URL")
-	if connectionString == "" {
-		connectionString = "postgres://Sinnott@localhost:5432/pwadb?sslmode=disable&timezone=UTC"
-	}
-	return connectionString
 }
---
+++ env/env.go
@@ -0,0 +1,30 @@
+package env
+
+import "os"
+
+// JWTSecret to be used in during authentication
+func JWTSecret() []byte {
+	env := os.Getenv("JWT_SECRET")
+	if env == "" {
+		env = "SECRET_SSSHHHHHHH"
+	}
+	return []byte(env)
+}
+
+// ConnectionURL get the database connection string from ENV Vars or used a default
+func ConnectionURL() string {
+	connectionString := os.Getenv("POSTGRES_URL")
+	if connectionString == "" {
+		connectionString = "postgres://Sinnott@localhost:5432/pwadb?sslmode=disable&timezone=UTC"
+	}
+	return connectionString
+}
+
+// Port retrieves the Port to start the server on
+func Port() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	return port
+}
