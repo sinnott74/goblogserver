edf3fdf85bb6f3c17626cfc7ef0d3dc24bf11741
Daniel Sinnott <sinnott74@hotmail.com>
2018-08-18 00:03:37 +0100

    uppercase check not needed

--- middleware/forceHTTPS.go
+++ middleware/forceHTTPS.go
@@ -8,7 +8,7 @@ import (
 func ForceHTTPS(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		proto := r.Header.Get("x-forwarded-proto")
-		if proto == "http" || proto == "HTTP" {
+		if proto == "http" {
 			http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusPermanentRedirect)
 			return
 		}
